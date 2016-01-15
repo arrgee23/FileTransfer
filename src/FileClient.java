4689fdf43df42004b27fbc41f31ef2780bc35efd
rahul <rahul@Lenovo>
2016-01-15 18:00:25 +0530

    1st fully working

---
+++ src/FileClient.java
@@ -0,0 +1,70 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class FileClient {  
+	Socket sock=null;
+	InputStream in=null;
+	OutputStream os = null;
+	DataOutputStream dos=null;
+	DataInputStream serverData = null;
+	OutputStream output = null;
+	
+	public FileClient() {
+		try {
+			sock = new Socket("127.0.0.1", 13267);
+			in = sock.getInputStream();
+			os = sock.getOutputStream();
+			System.out.println("Connected To: "+sock.getInetAddress());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	public void receive(){
+		try{
+			// scan filename
+			System.out.print("Enter the name of the file: ");
+			Scanner s = new Scanner(System.in);
+			String filename = s.next();
+			s.close();
+			
+			// send filename to the server
+			dos = new DataOutputStream(os);     
+			dos.writeUTF(filename);     
+
+			// read file size from server
+			serverData = new DataInputStream(in);
+			long size = serverData.readLong();     
+
+			// write to file in 1kb chunk
+			int bytesRead=0;
+			byte[] buffer = new byte[1024];     
+			output = new FileOutputStream("downloaded.mp3");
+			while (size > 0 && (bytesRead = serverData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)     
+			{   
+				output.write(buffer, 0, bytesRead);     
+				size -= bytesRead;     
+			}  
+			//Closing socket
+			in.close();
+			os.close();
+			dos.close();  
+			output.close();
+			sock.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)  {  
+		FileClient f = new FileClient();
+		f.receive();
+	}  
+}
---
+++ src/FileServer.java
@@ -0,0 +1,70 @@
+import java.net.*;     
+import java.io.*;     
+public class FileServer{
+
+	public static void main(String args[]) throws Exception
+	{
+		@SuppressWarnings("resource")
+		ServerSocket soc=new ServerSocket(13267);
+		System.out.println("FTP Server Started on Port Number 5217");
+		while(true)
+		{
+			System.out.println("Waiting for Connection ...");
+			new FileTransfer(soc.accept());
+		}
+	}
+}
+class FileTransfer extends Thread{  
+
+	Socket clientSocket = null;
+
+	public FileTransfer(Socket soc) {
+		try{
+			clientSocket = soc;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		start();
+	}
+	@Override
+	public void run() {    
+		try{
+			InputStream in = clientSocket.getInputStream();  
+			DataInputStream clientData = new DataInputStream(in);
+
+			// read filename
+			String filename = clientData.readUTF();
+			File myFile = new File(filename); // open file named filename
+
+			////////////////// file exists assumed /////////////////
+
+			// read from file into buffer
+			byte[] mybytearray = new byte[(int) myFile.length()];  
+			FileInputStream fis = new FileInputStream(myFile);  
+			BufferedInputStream bis = new BufferedInputStream(fis);  
+			DataInputStream dis = new DataInputStream(bis);     
+			dis.readFully(mybytearray, 0, mybytearray.length);  
+
+			OutputStream os = clientSocket.getOutputStream();  
+
+
+			//Sending file size to the client  
+			DataOutputStream dos = new DataOutputStream(os);          
+			dos.writeLong(mybytearray.length);
+
+			//Sending file data to the server  
+			os.write(mybytearray, 0, mybytearray.length);  
+			os.flush();  
+
+			os.close();
+			dos.close();
+			in.close();
+			clientData.close();
+			dis.close();
+			clientSocket.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}  
+} 
